d7390acfef6cc74673767b74983c3edb831ae058
Noah Serwin <serwinno13@uwosh.edu>
2026-05-08 09:44:27 -0500

    Added a line with my initials

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('hello world'));
 
+app.get('/nrs', (req,res) => res.send('hello from nrs'));
+
 app.listen(3000, () => { console.log('Listening on port 3000'); });
